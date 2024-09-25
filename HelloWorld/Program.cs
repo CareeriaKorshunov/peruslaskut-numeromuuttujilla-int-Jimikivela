cff448f8b4bc029d05217291bebcacd26c3c5229
Jimi Kivelä <jimi.kivela@student.careeria.fi>
2024-09-25 10:06:32 +0300

    tehtävänanto

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,7 +4,29 @@
     {
         public static void Main(string[] args)
         {
-           //koodi
+            //luku muuttujat
+            int luku1 = 10;
+            int luku2 = 5;
+
+            //tulos muuttujat
+            int tulo;
+            int erotus;
+            int summa;
+            int osamäärä;
+            
+            //annetaan arvot tulos muuttujille luku muuttujista
+            summa = luku1 + luku2;
+            erotus = luku1 - luku2;
+            tulo = luku1 * luku2;
+            osamäärä = luku1 / luku2;
+
+            //tulostetaan vastaukset
+            Console.WriteLine("Peruslaskujen tulokset:");
+            Console.WriteLine("Tulo: " + tulo);
+            Console.WriteLine("Summa: " + summa);
+            Console.WriteLine("Erotus: " + erotus);
+            Console.WriteLine("Osamäärä: " + osamäärä);
+
 
         }
     }
